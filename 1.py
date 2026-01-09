9b49a46ffa3b6cedab7bc2e1e279f8febc140b4c
Zaid-Khan-22 <itzzaid2215@gmail.com>
2026-01-09 09:37:20 +0530

    S

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-print("Hello")
+def rev(str):
+    revS = ""
+    for ch in str:
+        revS = ch + revS
+    return revS
+
+def e_o(num):
+    if num%2 == 0:
+        return "Even"
+    else:
+       return "Odd"
+
+print(e_o(6))
+print(rev("123"))
